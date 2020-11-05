20d823dcdab18d9148837df2ec3174f47bced256
Czarl Jalos <czarlfrancis@gmail.com>
2020-11-05 04:15:10 -0800

    lab-18

--- server/server.js
+++ server/server.js
@@ -1,47 +1,56 @@
-'use strict'
+'use strict';
 
 require('dotenv').config();
-const net = require('net')
-const port = process.env.PORT || 3000;
+const store = process.env.STORE;
+const io = require('socket.io')(process.env.PORT || 3000);
+// const { v4: uuid } = require('uuid');
 
-const server = net.createServer();
+// Create and accept connections on a namespace called caps
+const caps = io.of('/caps'); // our NAMESPACE
 
-server.listen(port, () => console.log(`Server up on ${port}`));
+// Message queue
+const messages = {};
 
+caps.on('connection', socket => {
+  console.log('Connected on: ', socket.id);
 
-const socketPool = {};
+  socket.on('join', room => {
+    console.log('Joined room: ', room);
+    socket.join(room);
+  });
 
-server.on('connection', socket => {
+  socket.on('received', orderID => {
+    delete messages[orderID];
+  });
 
-  let id = `Socket-${Math.random()}`;
+  // This is saying: "When someone broadcasts 'getall', we'll do this." - who should broadcast it? THE DRIVER! (whenever they connect)
+  socket.on('getall', () => {
+    for (let id in messages) {
+      const payload = messages[id];
+      caps.emit('pickup', payload);
+    }
+  });
 
-  socketPool[id] = socket;
-  
-  socket.on('data', buffer => dispatchEvent(buffer));
+  // We need to QUEUE UP pickup messages
+  socket.on('pickup', payload => {
+    messages[payload.orderID] = payload; 
+    eventLogger('pickup', payload);
+    caps.emit('pickup', payload);
+  });
 
-  socket.on('error', (e) => {console.error('SECKET ERROR', e)})
-  socket.on('end', (e) => { delete socketPool[id]; });
+  socket.on('in-transit', payload => {
+    eventLogger('in-transit', payload);
+    caps.to(store).emit('in-transit', payload);
+  });
 
-})
-
-server.on('error', (e) => {
-  console.error('SERVER ERROR', e.message);
+  socket.on('delivered', payload => {
+    eventLogger('delivered', payload);
+    caps.to(store).emit('delivered', payload);
+  });
 });
 
-
-
-
-function dispatchEvent(buffer) {
-  let message = buffer;
-
-  broadcast(message);
-}
-
-function broadcast(message) {
-  let payload = JSON.stringify(message);
-  for(let socket in socketPool) {
-    const socket = socketPool[socketId];
-    console.log(socket);
-    socket.write(payload);
-  }
+function eventLogger(event, payload) {
+  let time = new Date();
+  const eventObj = { event, time, payload };
+  console.log('[EVENT]: ', eventObj);
 }
